0c4c9667720403d08e18ee9fd9b88a4cfd46d832
1104622460 <1104622460@qq.com>
2022-06-12 23:57:26 +0800

    Setup premake and Log system

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -4,6 +4,7 @@
 //集成Hazel所以原程序的链接
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 //#include <stdio.h>
 // ---Entry Point-----------------------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 //extern 修饰全局变量 表明该全局常量拥有外部链接
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
